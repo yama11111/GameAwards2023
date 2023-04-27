7f9bb6b52a1302ebc7175f6fcd4cdd34e62e36fc
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-04-27 11:20:11 +0900

    とりあえず、puzmatクラス追加したよ

---
+++ PuzzleMat.cpp
@@ -0,0 +1,37 @@
+#include "PuzzleMat.h"
+
+PuzzleMat::PuzzleMat(int pieceX, int pieceY)
+{
+    for (int y = 0; y < pieceY; y++) 
+    {
+        puzMat_.emplace_back(std::vector<Pises>{});
+
+        for (int x = 0; x < pieceX; x++)
+        {
+            puzMat_[y].emplace_back(Pises{});
+            puzMat_[y][x].Initialize();
+        }
+    }
+}
+
+void PuzzleMat::Update(void)
+{
+    for (auto y = 0; y < puzMat_.size(); y++)
+    {
+        for (auto x= 0; x < puzMat_[0].size(); x++)
+        {
+            puzMat_[y][x].Update();
+        }
+    }
+}
+
+void PuzzleMat::Draw(void)
+{
+    for (auto y = 0; y < puzMat_.size(); y++)
+    {
+        for (auto x = 0; x < puzMat_[0].size(); x++)
+        {
+            puzMat_[y][x].Draw();
+        }
+    }
+}
---
+++ PuzzleMat.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Pises.h"
+
+class PuzzleMat
+{
+public:
+    // ŠÖ”
+    PuzzleMat(int pieceX, int pieceY);
+    void Update(void);
+    void Draw(void);
+
+private:
+    // •Ï”
+    std::vector<std::vector<Pises>> puzMat_;
+
+public:
+    // setterEgetter
+    inline const std::vector<std::vector<Pises>>& GetPuzMat(void) { return puzMat_; }
+};
+
